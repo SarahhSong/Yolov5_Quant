194486bfdd03af02ce2131493da1b70720c9d4e5
SarahhSong <songzySarah@163.com>
2023-07-21 12:09:04 +0800

    modify train.py to support send args

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ import sys
 sys.path.append('../Yolov5_Quant')
 sys.path.insert(0,"/content/drive/MyDrive/Colab Notebooks/python_packages/")
 
-
+import argparse
 import os
 import tqdm
 import numpy as np
@@ -22,25 +22,108 @@ from layers import nms
 from utils.printModel import summary
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+logging = mge.logger.get_logger()
 
 
 def main():
-    epochs = 5
-    log_dir = './logs'
-    # 可以选择 ['5l', '5s', '5m', '5x']
-    yolov5_type = "5s"
-    image_shape = (320, 320, 3)
-    num_class = 91
-    # num_class = 2
-    batch_size = 8
-    # -1表示全部数据参与训练
-    train_img_nums = 500
-    train_coco_json = '/mnt/e/数据集/COCO/annotations/annotations_trainval2017/annotations/instances_train2017.json'
-    val_coco_json = '/mnt/e/数据集/COCO/annotations/annotations_trainval2017/annotations/instances_val2017.json'
-    train_coco_data = '/mnt/e/数据集/COCO/images/train2017/train2017'
-    val_coco_data = '/mnt/e/数据集/COCO/images/val2017/val2017'
+    parser = argparse.ArgumentParser(description="MegEngine COCO Q_yolov5s Training")
+    # parser.add_argument("-d", "--data", metavar="DIR", help="path to imagenet dataset")
+    parser.add_argument(
+        "-a",
+        "--arch",
+        default="5s",
+        help="model architecture (default: 5s)",
+    )
+    parser.add_argument(
+        "-m", "--model", metavar="PKL", default=None, help="path to model checkpoint"
+    )
+    parser.add_argument(
+        "--save",
+        metavar="DIR",
+        default="./logs",
+        help="path to save checkpoint and log",
+    )
+    parser.add_argument(
+        "--epochs",
+        default=300,
+        type=int,
+        help="number of total epochs to run (default: 300)",
+    )
 
-    # 类别名, 也可以自己提供一个数组, 不通过coco
+    parser.add_argument(
+        "-b",
+        "--batch-size",
+        metavar="SIZE",
+        default=32,
+        type=int,
+        help="batch size for single GPU (default: 32)",
+    )
+    parser.add_argument(
+        "-n",
+        "--img-num",
+        default=-1,
+        type=int,
+        help="image nums for training (default: -1)",
+    )
+    parser.add_argument(
+        "-s",
+        "--img-shape",
+        default=(320, 320, 3),
+        type=tuple,
+        help="image shape for model imput" 
+    )   
+    parser.add_argument(
+        "--lr",
+        "--learning-rate",
+        metavar="LR",
+        default=0.0001,
+        type=float,
+        help="learning rate for single GPU (default: 0.0001)",
+    )
+    # parser.add_argument(
+    #     "--momentum", default=0.9, type=float, help="momentum (default: 0.9)"
+    # )
+    # parser.add_argument(
+    #     "--weight-decay", default=1e-4, type=float, help="weight decay (default: 1e-4)"
+    # )
+    parser.add_argument(
+        "--mode",
+        default="normal",
+        type=str,
+        choices=["normal", "qat"],
+        help="Quantization Mode\n"
+        "normal: no quantization, using float32\n"
+        "qat: quantization aware training, simulate int8\n"
+    )
+    parser.add_argument(
+        "--train-json",
+        default='/mnt/e/数据集/COCO/annotations/annotations_trainval2017/annotations/instances_train2017.json',
+        type=str,
+        help="dir for train_data_json (default: )" 
+    )       
+    parser.add_argument(
+        "--val-json",
+        default='/mnt/e/数据集/COCO/annotations/annotations_trainval2017/annotations/instances_val2017.json',
+        type=str,
+        help="dir for val_data_json (default: )" 
+    )
+    parser.add_argument(
+        "--train-data",
+        default='/mnt/e/数据集/COCO/images/train2017/train2017',
+        type=str,
+        help="dir for train_data (default: )" 
+    )
+    parser.add_argument(
+        "--val-data",
+        default='/mnt/e/数据集/COCO/images/val2017/val2017',
+        type=str,
+        help="dir for val_data (default:)" 
+    )             
+    args = parser.parse_args()
+    worker(args)
+    
+
+def worker(args):
     classes = ['none', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
                 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'none', 'stop sign',
                 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant',
@@ -52,7 +135,7 @@ def main():
                 'dining table', 'none', 'none', 'toilet', 'none', 'tv', 'laptop', 'mouse', 'remote', 'keyboard',
                 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'none', 'book', 'clock',
                 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
-    # classes = ['cat', 'dog']
+    num_class = len(classes)
 
     # 这里anchor归一化到[0,1]区间
     anchors = np.array([[8, 10], [21, 26], [33, 64],
@@ -62,62 +145,42 @@ def main():
     # 分别对应1/8, 1/16, 1/32预测输出层
     anchor_masks = np.array([[0, 1, 2], [3, 4, 5], [6, 7, 8]], dtype=np.int8)
     # tensorboard日志
-    summary_writer = tf.summary.create_file_writer(log_dir)
     # data generator
     
-    coco_data = CoCoDataGenrator(
-        coco_annotation_file= train_coco_json,
-        coco_data_file = train_coco_data,
-        train_img_nums=train_img_nums,
-        img_shape=image_shape,
-        batch_size=batch_size,
-        include_mask=False,
-        include_crowd=False,
-        include_keypoint=False,
-        need_down_image=False,
-        using_argument=True
-    )
-    # 验证集
-    val_coco_data = CoCoDataGenrator(
-        coco_annotation_file=val_coco_json,
-        coco_data_file = val_coco_data,
-        train_img_nums=-1,
-        img_shape=image_shape,
-        batch_size=batch_size,
-        include_mask=False,
-        include_crowd=False,
-        include_keypoint=False,
-        need_down_image=False,
-        using_argument=False
-    )
-    
     yolo = Yolo(
-        image_shape=image_shape,
-        batch_size=batch_size,
+        image_shape=args.img_shape,
+        batch_size=args.batch_size,
         num_class=num_class,
         is_training=True,
         anchors=anchors,
         anchor_masks=anchor_masks,
-        net_type=yolov5_type
+        net_type=args.arch
     )
-    yolo.yolov5.named_modules
+
     model = yolo.yolov5
 
-    model = convert_qat(model)
+    if args.model is not None:
+        logging.info("load from checkpoint %s", args.model)
+        checkpoint = mge.load(args.model)
+        if "state_dict" in checkpoint:
+            state_dict = checkpoint["state_dict"]
+        else:
+            state_dict = checkpoint
+        model.load_state_dict(state_dict)
 
-    # checkpoint = mge.load("./logs/yolov5s-best(img:500).pkl")
-    # model.load_state_dict(checkpoint["state_dict"], strict=False)
-    # model = quantize(model_qat)
-    # print(model)
-    # with open('model.txt', 'w') as f:
-    #     for k, v in model.named_parameters():
-    #         f.write("{}\t{}\n".format(k, v))
+    if args.mode == "qat":
+        model = convert_qat(model)
 
-    # summary(yolo.yolov5,(3,640,640))
-    optimizer = optim.Adam(model.parameters() , lr = 0.0001)
+    # if args.mode == "quantize":
+    #     model = quantize(model) 
+    #     with open('model.txt', 'w') as f:
+    #         for k, v in model.named_parameters():
+    #             f.write("{}\t{}\n".format(k, v))
+
+    optimizer = optim.Adam(model.parameters() , args.lr)
 
     loss_fn = ComputeLoss(
-        image_shape=image_shape,
+        image_shape=args.img_shape,
         anchors=anchors,
         anchor_masks=anchor_masks,
         num_class=num_class,
@@ -128,9 +191,12 @@ def main():
     )
 
     pre_mAP = 0.
-    # data = coco_data.next_batch()
-    for epoch in range(epochs):
-        train_progress_bar = tqdm.tqdm(range(coco_data.total_batch_size), desc="train epoch {}/{}".format(epoch, epochs-1), ncols=100)
+
+    coco_data, val_coco_data = build_datasets(args)
+    summary_writer = tf.summary.create_file_writer(args.save)
+
+    for epoch in range(args.epochs):
+        train_progress_bar = tqdm.tqdm(range(coco_data.total_batch_size), desc="train epoch {}/{}".format(epoch, args.epochs-1), ncols=100)
         for batch in train_progress_bar:
             with GradManager() as gm:
                 gm.attach(model.parameters())
@@ -138,7 +204,7 @@ def main():
                 valid_nums = data['valid_nums']
                 gt_imgs = np.array(data['imgs'] / 255., dtype=np.float32)
                 gt_imgs = mge.Tensor(gt_imgs)
-                gt_boxes = np.array(data['bboxes'] / image_shape[0], dtype=np.float32)
+                gt_boxes = np.array(data['bboxes'] / args.img_shape[0], dtype=np.float32)
                 gt_classes = data['labels']
 
                 # print("-------epoch {}, step {}, total step {}--------".format(epoch, batch,
@@ -156,12 +222,12 @@ def main():
                 total_loss = loss_box + loss_obj + loss_cls
                 train_progress_bar.set_postfix(ordered_dict={"loss":'{:.5f}'.format(total_loss)})
 
-                # grad = tape.gradient(total_loss, yolo.yolov5.trainable_variables)
                 gm.backward(mge.Tensor(np.array(total_loss)))
+
                 with open('grad.txt', 'w') as f:
                     for name, paramer in model.named_parameters():
                         f.write("-->name:{}\t-->grad_value:{}\n".format(name, paramer.grad))
-                # optimizer.apply_gradients(zip(grad, yolo.yolov5.trainable_variables))
+
                 optimizer.step().clear_grad()
 
                 # Scalar
@@ -176,11 +242,11 @@ def main():
                                       step=epoch * coco_data.total_batch_size + batch)
 
                 # # image, 只拿每个batch的其中一张
-                random_one = random.choice(range(batch_size))
+                random_one = random.choice(range(args.batch_size))
                 # gt
                 gt_imgs = gt_imgs.numpy()
                 gt_img = gt_imgs[random_one].copy() * 255
-                gt_box = gt_boxes[random_one] * image_shape[0]
+                gt_box = gt_boxes[random_one] * args.img_shape[0]
                 gt_class = gt_classes[random_one]
                 non_zero_ids = np.where(np.sum(gt_box, axis=-1))[0]
                 for i in non_zero_ids:
@@ -193,8 +259,8 @@ def main():
                 # # pred, 同样只拿第一个batch的pred
                 pred_img = gt_imgs[random_one].copy() * 255
                 yolo_head_output = yolo.yolo_head(yolo_preds, is_training=False)
-                nms_output = nms(image_shape, yolo_head_output.numpy(), iou_thres=0.3)
-                if len(nms_output) == batch_size:
+                nms_output = nms(args.img_shape, yolo_head_output.numpy(), iou_thres=0.3)
+                if len(nms_output) == args.batch_size:
                     nms_output = nms_output[random_one]
                     for box_obj_cls in nms_output:
                         if box_obj_cls[4] > 0.5:
@@ -224,8 +290,8 @@ def main():
                 "state_dict": model.state_dict(),
                 "optimizer_state_dict": optimizer.state_dict(),
                 "mAP": mAP
-                }, './logs/yolov{}-best.pkl'.format(yolov5_type))
-            print("save {}/yolov{}-best.pkl best weight with {} mAP.".format(log_dir, yolov5_type, mAP))
+                }, './logs/yolov{}-best.pkl'.format(args.arch))
+            print("save {}/yolov{}-best.pkl best weight with {} mAP.".format(args.save, args.arch, mAP))
         # yolo.yolov5.save_weights(log_dir+"/yolov{}-last.h5".format(yolov5_type))
 
         if epoch % 10 == 0:
@@ -234,8 +300,38 @@ def main():
                 "state_dict": model.state_dict(),
                 "optimizer_state_dict": optimizer.state_dict(),
                 "mAP": mAP
-                }, './logs/yolov{}-epoch{}.pkl'.format(yolov5_type, epoch))
-          print("save {}/yolov{}-epcho.pkl last weights at epoch {}.".format(log_dir, yolov5_type, epoch))
+                }, './logs/yolov{}-epoch{}.pkl'.format(args.arch, epoch))
+          print("save {}/yolov{}-epcho.pkl last weights at epoch {}.".format(args.save, args.arch, epoch))
+
+
+def build_datasets(args):
+    # 训练集
+    coco_data = CoCoDataGenrator(
+        coco_annotation_file= args.train_json,
+        coco_data_file = args.train_data,
+        train_img_nums=args.img_num,
+        img_shape=args.img_shape,
+        batch_size=args.batch_size,
+        include_mask=False,
+        include_crowd=False,
+        include_keypoint=False,
+        need_down_image=False,
+        using_argument=True
+    )
+    # 验证集
+    val_coco_data = CoCoDataGenrator(
+        coco_annotation_file=args.val_json,
+        coco_data_file = args.val_data,
+        train_img_nums=args.img_num,
+        img_shape=args.img_shape,
+        batch_size=args.batch_size,
+        include_mask=False,
+        include_crowd=False,
+        include_keypoint=False,
+        need_down_image=False,
+        using_argument=False
+    )
+    return coco_data, val_coco_data
 
 
 if __name__ == "__main__":
